d90ebb75f2fbbf34f8920ce5fdcc4cb2ca0236cf
carolcampos22 <mendes.carolina2209@gmail.com>
2025-03-15 20:26:46 -0300

    Formatação de data e atualização da logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
 import Image from 'next/image';
 import styles from '../app/styles/Header.module.scss';
-import logo from '../public/Logomark.png'
+import logo from '../public/note.jpg'
 
 export const Header = () => {
+    const currentDate = new Date()
+    
+    const daysOfTheWeek = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+    const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+
+    const dayOfTheWeek = daysOfTheWeek[currentDate.getDay()]
+    
+    const day = currentDate.getDate().toString().padStart(2, '0')
+    const month = months[currentDate.getMonth()]
+    const year = currentDate.getFullYear()
+
+    const formattedDate = `${dayOfTheWeek}, ${day} de ${month} de ${year}`
+    
+
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
-                <Image src={logo} alt="Logo da FocalPoint" />
-                <h1>FocalPoint</h1>
+                <Image src={logo} alt="Logo da UpTask" />
             </div>
-            <p>Bem-vindo de volta, Marcus</p>
-            <span>Segunda, 01 de dezembro de 2025</span>
+            <p>UpTask</p>
+            <span>{formattedDate}</span>
         </div>
     );
 };
